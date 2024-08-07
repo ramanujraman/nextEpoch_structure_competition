c0301abb2a7496fcbe299d22aaf2309bbcd9f575
Ramanuj Raman <ramanujraman2003@gmail.com>
2024-08-08 02:01:39 +0530

    Reset

--- train.py
+++ train.py
@@ -4,66 +4,72 @@ from src.util import compute_f1, compute_precision, compute_recall, plot_structu
 from src.submission_formatter import format_submission
 
 import torch
+import torch.nn as nn
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Load data
-train_loader, val_loader, test_loader = get_dataloaders(batch_size=2, max_length=50, split=0.8, max_data=1000)
+train_loader, val_loader, test_loader = get_dataloaders(batch_size = 8, max_length=70, split=0.8, max_data=1000)
 
 # Init model, loss function, optimizer
-model = RNA_net(embedding_dim=32).to(device)
-criterion = torch.nn.BCEWithLogitsLoss()
+model = RNA_net(embedding_dim=64)
+criterion = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([300]))
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 # Training loop
+train_losses = []
+valid_losses = []
+f1s_train = []
+f1s_valid = []
 for epoch in range(10):
 
-    model.train()
-    loss_epoch = 0.0
-    metric_epoch = 0.0
+    loss_train = 0.0
+    f1_train = 0.0
+    loss_valid = 0.0
+    f1_valid = 0.0
 
     for batch in train_loader:
 
-        x = batch["sequence"].to(device) # (N, L)
-        y = batch['structure'].to(device) # (N, L, L)
+        x = batch["sequence"] # (N, L)
+        y = batch['structure'] # (N, L, L)
 
         y_pred = model(x)
 
         loss = criterion(y_pred, y)
         optimizer.zero_grad()
         loss.backward()
+
         optimizer.step()
 
-        loss_epoch += loss.item()
-        metric_epoch += compute_f1(y_pred, y)
+        loss_train += loss.item()
+        f1_train += compute_f1(y_pred, y)
 
-    print(f"Epoch {epoch+1}: Loss {loss_epoch/len(train_loader):.4f}, F1 score {metric_epoch/len(train_loader):.4f}")
+    for batch in val_loader:
+        x = batch["sequence"] # (N, L)
+        y = batch['structure'] # (N, L, L)
 
-    # Validation loop
-    model.eval()
-    val_loss = 0.0
-    val_metric = 0.0
-    with torch.no_grad():
-        for batch in val_loader:
-            x = batch["sequence"].to(device) # (N, L)
-            y = batch['structure'].to(device) # (N, L, L)
+        y_pred = model(x)
 
-            y_pred = model(x)
+        loss = criterion(y_pred, y)
 
-            loss = criterion(y_pred, y)
-            val_loss += loss.item()
-            val_metric += compute_f1(y_pred, y)
+        loss_valid += loss.item()
+        f1_valid += compute_f1(y_pred, y)
 
-    print(f"Validation Loss: {val_loss/len(val_loader):.4f}, Validation F1 score: {val_metric/len(val_loader):.4f}")
+    train_losses.append(loss_train/len(train_loader))
+    valid_losses.append(loss_valid/len(val_loader))
+
+    f1s_train.append(f1_train/len(train_loader))
+    f1s_valid.append(f1_valid/len(val_loader))
+
+    print(f"Epoch {epoch}, F1 train: {f1s_train[-1]:.2f}, F1 valid: {f1s_valid[-1]:.2f}")
+
+
+# Validation loop
 
 # Test loop
-model.eval()
 structures = []
-with torch.no_grad():
-    for sequence in test_loader[1]:
-        x = sequence.to(device) # Move sequence to device
-        y_pred = model(x)
-        structure = (torch.sigmoid(y_pred) > 0.5).type(torch.int) # Convert logits to binary predictions
-        structures.append(structure.cpu()) # Move structure back to CPU
+for sequence in test_loader[1]:
+    # Replace with your model prediction !
+    structure = (model(sequence.unsqueeze(0)).squeeze(0)>0.5).type(torch.int) # Has to be shape (L, L) ! 
+    structures.append(structure)
 
 format_submission(test_loader[0], test_loader[1], structures, 'test_pred.csv')
